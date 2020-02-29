6ba530de989ffd9ffd8b9154cfc1faa1b114f36d
EvilMilko <kostax2012@gmail.com>
2020-02-29 16:35:27 +0500

    Подключена библиотека Route
    Ссылки в navbar теперь работают
    Добавлены страницы News. Music. Settings.

--- src/App.js
+++ src/App.js
@@ -5,19 +5,30 @@ import Header from './components/Header/Header';
 import Profile from './components/Profile/Profile';
 import NavBar from './components/NavBar/NavBar';
 import Dialogs from './components/Dialogs/Dialogs';
+import News from './components/News/News';
+import Music from './components/Music/Music';
+import Settings from './components/Settings/Settings';
+import { Route, BrowserRouter } from 'react-router-dom';
+
 
 const App = () => {
   return (
-    <div className="app-wrapper" >
-      <Header />
-      <NavBar />
-      <div className='app-wrapper-content'>
-        {/* <Profile /> */}
-        <Dialogs />
+    <BrowserRouter>
+      <div className="app-wrapper" >
+        <Header />
+        <NavBar />
+        <div className='app-wrapper-content'>
+          <Route path="/profile" component={Profile} />
+          <Route path="/dialogs" component={Dialogs} />
+          <Route path="/news" component={News} />
+          <Route path="/music" component={Music} />
+          <Route path="/settings" component={Settings} />
 
+        </div>
       </div>
-    </div>
+    </BrowserRouter>
   );
+
 }
 
 export default App;
--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,14 +1,25 @@
 import React from 'react';
+
 import cm from './NavBar.module.css'
 const NavBar = () => {
 
     return (
         <nav className={cm.nav}>
-            <div className={cm.item}>Profile</div>
-            <div className={cm.item}>Messages</div>
-            <div className={cm.item}>News</div>
-            <div className={cm.item}>Music</div>
-            <div className={cm.item}>Settings</div>
+            <div className={cm.item}>
+                <a href="/profile">Profile</a>
+            </div>
+            <div className={cm.item}>
+                <a href="/dialogs">Messages</a>
+            </div>
+            <div className={cm.item}>
+                <a href="/news">News</a>
+            </div>
+            <div className={cm.item}>
+                <a href="/music">Music</a>
+            </div>
+            <div className={cm.item}>
+                <a href="/settings">Settings</a>
+            </div>
         </nav>
 
     )
