2196fce1c08aefe91cca97a21f7e539813115325
EvilMilko <kostax2012@gmail.com>
2020-03-01 15:44:00 +0500

    Получение Data
    Добавлено получение Data в Dialogs и Profile

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -18,21 +18,37 @@ const Message = (props) => {
     )
 }
 
+let dialogsData = [
+    { id: 1, name: "Dimych1" },
+    { id: 2, name: "Dimych2" },
+    { id: 3, name: "Dimych3" },
+    { id: 4, name: "Dimych4" },
+    { id: 5, name: "Dimych5" },
+]
+
+let messagesData = [
+    { id: 1, message: 'text1' },
+    { id: 2, message: 'text2' },
+    { id: 3, message: 'text3' },
+    { id: 4, message: 'text4' },
+    { id: 5, message: 'text5' },
+]
+
 const Dialogs = (props) => {
     return (
         <div>
             <div className={cm.dialogs}>
 
                 <div className={cm.dialogsItems}>
-                    <DialogItem id='1' name='Dimych' />
-                    <DialogItem id='2' name='Dimych2' />
+                    <DialogItem id={dialogsData[0].id} name={dialogsData[0].name} />
+                    <DialogItem id={dialogsData[1].id} name={dialogsData[1].name} />
 
                 </div>
 
                 <div className={cm.messages}>
-                    <Message message="Test1" />
-                    <Message message="Test2" />
-                    <Message message="Test3" />
+                    <Message id={messagesData[0].id} message={messagesData[0].message} />
+                    <Message id={messagesData[1].id} message={messagesData[1].message} />
+
 
                 </div>
 
--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -2,6 +2,10 @@ import React from 'react';
 import cm from './MyPosts.module.css'
 import Post from './Posts/Post';
 
+let postData = [
+    { id: 1, message: "First post", likescount: 5 },
+    { id: 2, message: "Bleat", likescount: 1 },
+]
 
 const MyPosts = () => {
 
@@ -18,8 +22,9 @@ const MyPosts = () => {
             </div>
 
             <div className={cm.posts}>
-                <Post likescount="5" message="text" />
-                <Post likescount="2" message="text2" />
+                <Post likescount={postData[1].likescount} message={postData[1].message} />
+                <Post likescount={postData[0].likescount} message={postData[0].message} />
+
             </div>
         </div>
     )
