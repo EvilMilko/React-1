be99e0a0bb5d4add0b46a27cf5e98a6543596907
EvilMilko <kostax2012@gmail.com>
2020-03-07 20:16:01 +0500

    Fix #2
    Исправлен клик по диалогам, теперь можно кликать не только по имени.

--- src/components/Dialogs/DialogItem/DialogItem.jsx
+++ src/components/Dialogs/DialogItem/DialogItem.jsx
@@ -6,10 +6,11 @@ import { NavLink } from 'react-router-dom';
 const DialogItem = (props) => {
     let path = '/dialogs/' + props.id
     return (
-        <div className={cm.dialog}>
-            <img src="https://cdn.worldvectorlogo.com/logos/github-icon.svg" alt=""/>
-            <NavLink activeClassName={cm.active} to={path}>{props.name} </NavLink>
-        </div>
+        <NavLink className={cm.dialog} activeClassName={cm.active} to={path}>
+            <img src="https://cdn.worldvectorlogo.com/logos/github-icon.svg" alt="" />
+            <div>{props.name}</div>
+
+        </NavLink>
     )
 }
 
